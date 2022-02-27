33ff1ca1a8d8cfdd31e3fd16c7b502913d35eb54
David <david_gunawan_@hotmail.com>
2022-02-27 19:24:42 +0700

    Create TextField for TextEntry

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cuaca/utilities/constants.dart';
 import 'package:cuaca/services/weather.dart';
 
+import 'city_screen.dart';
+
 class LocationScreen extends StatefulWidget {
   LocationScreen({this.locationWeather});
   final locationWeather;
@@ -71,7 +73,11 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return CityScreen();
+                      }));
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
